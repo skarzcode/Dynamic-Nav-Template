a65baef6f1dfd54af2b3db72db4ad23394876057
Abdi <letsbuildshhopify>
2022-03-26 17:49:29 +0000

    Contact form and scroll animations

--- script.js
+++ script.js
@@ -117,6 +117,79 @@ const navFour = {
 
 },false));
 
+// scroll animation
+
+window.addEventListener("scroll", reveal);
+window.addEventListener("scroll", reveal2);
+window.addEventListener("scroll", reveal3);
+window.addEventListener("scroll", reveal4);
+// Translate vertical animation
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal")
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var revealTOP = reveals[i].getBoundingClientRect().top;
+        var revealPoint = 150;
+
+        if (revealTOP < windowHeight - revealPoint) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+// Opacity animation
+function reveal2() {
+    var reveals2 = document.querySelectorAll(".reveal2")
+    for (var i = 0; i < reveals2.length; i++) {
+        var windowHeight = window.innerHeight;
+        var revealTOP = reveals2[i].getBoundingClientRect().top;
+        var revealPoint = 150;
+
+        if (revealTOP < windowHeight - revealPoint) {
+            reveals2[i].classList.add("active2");
+
+        } else {
+            reveals2[i].classList.remove("active2");
+        }
+    }
+};
+
+// Translate Horizontal animation
+function reveal3() {
+    var reveals3 = document.querySelectorAll(".reveal3")
+    for (var i = 0; i < reveals3.length; i++) {
+        var windowHeight = window.innerHeight;
+        var revealTOP = reveals3[i].getBoundingClientRect().top;
+        var revealPoint = 150;
+
+        if (revealTOP < windowHeight - revealPoint) {
+            reveals3[i].classList.add("active3");
+
+        } else {
+            reveals3[i].classList.remove("active3");
+        }
+    }
+};
+
+// Translate Horizontal animation2
+function reveal4() {
+    var reveals4 = document.querySelectorAll(".reveal4")
+    for (var i = 0; i < reveals4.length; i++) {
+        var windowHeight = window.innerHeight;
+        var revealTOP = reveals4[i].getBoundingClientRect().top;
+        var revealPoint = 150;
+
+        if (revealTOP < windowHeight - revealPoint) {
+            reveals4[i].classList.add("active4");
+
+        } else {
+            reveals4[i].classList.remove("active4");
+        }
+    }
+};
+
 
 
 
